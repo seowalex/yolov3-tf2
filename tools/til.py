686e58cce126a0567543bbf52ce32f603a5b2035
Seow Alex <seowalex@hotmail.com>
2020-06-19 09:46:28 +0800

    Cleanup

--- tools/til.py
+++ tools/til.py
@@ -1,17 +1,16 @@
 import os
 import json
-import cv2
 
 from absl import app, flags, logging
 from absl.flags import FLAGS
+import cv2
 import tensorflow as tf
-import tqdm
+from tqdm import tqdm
 
-flags.DEFINE_string('data_dir', '..',
-                    'path to raw TIL dataset')
-flags.DEFINE_enum('split', 'train', [
-                  'train', 'val'], 'specify train or val spit')
+flags.DEFINE_string('data_dir', '..', 'path to raw TIL dataset')
+flags.DEFINE_enum('split', 'train', ['train', 'val'], 'specify train or val spit')
 flags.DEFINE_string('output_file', './data/til.tfrecord', 'output dataset')
+flags.DEFINE_string('classes', './data/til.names', 'classes file')
 
 def transform_annotations(annotations):
   tf_annotations = {}
@@ -29,13 +28,12 @@ def transform_annotations(annotations):
 
   return tf_annotations
 
-def build_example(image_id, annotation):
-  img_path = os.path.join(FLAGS.data_dir, FLAGS.split, FLAGS.split, str(image_id) + '.jpg')
+def build_example(image_id, annotation, classes):
+  img_path = os.path.join(FLAGS.data_dir, FLAGS.split, FLAGS.split, '{}.jpg'.format(image_id))
 
   with open(img_path, 'rb') as f:
     img_raw = f.read()
     im = cv2.imread(img_path)
-    classes = ['tops', 'trousers', 'outwear', 'dresses', 'skirts']
 
     height, width, _ = im.shape
 
@@ -64,19 +62,22 @@ def build_example(image_id, annotation):
     return example
 
 def main(_argv):
-  with open(os.path.join(FLAGS.data_dir, FLAGS.split + '.json')) as f:
+  with open(os.path.join(FLAGS.data_dir, '{}.json'.format(FLAGS.split))) as f:
     data = json.load(f)
     annotations = data['annotations']
 
-    writer = tf.io.TFRecordWriter(FLAGS.output_file)
-    tf_annotations = transform_annotations(annotations)
-    
-    for image_id, annotation in tqdm.tqdm(tf_annotations.items()):
-      tf_example = build_example(image_id, annotation)
-      writer.write(tf_example.SerializeToString())
-    
-    writer.close()
-    logging.info("Done")
+    with open(FLAGS.classes) as g:
+      classes = g.read().splitlines()
+
+      writer = tf.io.TFRecordWriter(FLAGS.output_file)
+      tf_annotations = transform_annotations(annotations)
+
+      for image_id, annotation in tqdm(tf_annotations.items()):
+        tf_example = build_example(image_id, annotation, classes)
+        writer.write(tf_example.SerializeToString())
+
+      writer.close()
+      logging.info('Done')
 
 if __name__ == '__main__':
     app.run(main)
